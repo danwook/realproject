c90dd06c219a12f9e50923b55b0f6ce125faa4c4
danwook <k.danwoo91@ibksystem.co.kr>
2020-02-19 09:19:55 +0900

    Initial commit

---
+++ src/main/java/com/example/realproject/RealprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.realproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealprojectApplication.class, args);
+    }
+
+}
